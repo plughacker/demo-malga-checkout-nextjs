5ae5e5c47660aea9fb377801c5a8c1c4dd6535e4
Leonardo Pinheiro <leoprocha100@gmail.com>
2021-11-25 12:09:33 -0300

    feat(app): create demo

---
+++ components/PlugCheckout.tsx
@@ -0,0 +1,65 @@
+import Dynamic from "next/dynamic";
+
+const PlugCheckoutWebComponent = Dynamic(() => import("@plug-checkout/react"), {
+  ssr: false,
+  loading: () => <p>Loading...</p>,
+});
+
+export function PlugCheckout() {
+  return (
+    <PlugCheckoutWebComponent
+      sandbox
+      publicKey="<YOUR_PUBLIC_KEY>"
+      clientId="<YOUR_CLIENT_ID>"
+      merchantId="<YOUR_MERCHANT_ID"
+      paymentMethods={{
+        pix: {
+          expiresIn: 600,
+        },
+        credit: {
+          installments: {
+            quantity: 1,
+            show: true,
+          },
+          showCreditCard: true,
+        },
+        boleto: {
+          expiresDate: "2022-12-31",
+          instructions: "Instruções para pagamento do boleto",
+          interest: {
+            days: 1,
+            amount: 100,
+          },
+          fine: {
+            days: 2,
+            amount: 200,
+          },
+        },
+      }}
+      transactionConfig={{
+        statementDescriptor: "#1 Demonstration Plug Checkout",
+        amount: 100,
+        description: "",
+        orderId: "",
+        customerId: "",
+        currency: "BRL",
+        capture: false,
+      }}
+      dialogConfig={{
+        show: true,
+        actionButtonLabel: "Continuar",
+        errorActionButtonLabel: "Tentar novamente",
+        successActionButtonLabel: "Continuar",
+        successRedirectUrl: "https://www.plugpagamentos.com/",
+        pixFilledProgressBarColor: "#344383",
+        pixEmptyProgressBarColor: "#D8DFF0",
+      }}
+      onPaymentSuccess={(data) => {
+        // Your specifications here
+      }}
+      onPaymentFailed={(error) => {
+        // Your specifications here
+      }}
+    />
+  );
+}
